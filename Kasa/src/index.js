e6c744b62faf399aff92ec5a14496952670a641e
Valérian M <valerian.mermoz@gmail.com>
2023-07-29 19:41:13 +0200

    finalisation
    last commit

--- Kasa/src/index.js
+++ Kasa/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Composants/Header/Header";
 import Footer from "./Composants/Footer/Footer";
@@ -28,4 +27,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+
