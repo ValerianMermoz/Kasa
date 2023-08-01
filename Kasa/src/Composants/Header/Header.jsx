9f9d7129c07890cc399f85000ba01fecf0fa7e12
Valérian M <valerian.mermoz@gmail.com>
2023-08-01 13:15:45 +0200

    Ajout classe underline liens

--- Kasa/src/Composants/Header/Header.jsx
+++ Kasa/src/Composants/Header/Header.jsx
@@ -1,19 +1,22 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom';
 import './Header.scss';
 import Logo from "../../Images/Logo.png";
 
 
+
 function Header() {
+    const lienActif = useLocation();
+
     return (
         <div className='Header'>
         <><div className="Header-logo">
         <img src={Logo} className='Logo' alt="Logo Kasa" />
         </div>
         <nav className="headernav">
-                <Link to="/">Accueil</Link>
-                <Link to="/About">A Propos</Link>
+                <Link to="/" className={`link ${lienActif.pathname === '/' ? 'actif' : ''}`}>Accueil</Link>
+                <Link to="/About" className={`link ${lienActif.pathname === '/About' ? 'actif' : ''}`}>A Propos</Link>
             </nav></>
             </div>
     )
 }
-export default Header 
+export default Header;
